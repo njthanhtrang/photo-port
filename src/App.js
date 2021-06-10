d9a8c3441e9a6d2b144c30a37c6d15d16fc2d990
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-06-10 11:02:02 -0700

    type error, mime.lookup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Nav from "./components/Nav";
 import About from "./components/About";
+import Gallery from "./components/Gallery";
 import './App.css';
 
 // JSX
@@ -10,6 +11,7 @@ function App() {
     <div>
       <Nav></Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
